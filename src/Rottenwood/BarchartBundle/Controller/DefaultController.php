e98469e0255ad11d0f3be43fa0a34aa97f424d36
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-09-05 23:32:10 +0400

    Интеграция парсера в symfony

--- src/Rottenwood/BarchartBundle/Controller/DefaultController.php
+++ src/Rottenwood/BarchartBundle/Controller/DefaultController.php
@@ -4,10 +4,15 @@ namespace Rottenwood\BarchartBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('RottenwoodBarchartBundle:Default:index.html.twig');
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        $data = array();
+
+        $service = $this->get("barchart.parser");
+
+        var_dump($service->getPrice("ESU4"));
+
+        return $this->render('RottenwoodBarchartBundle:Default:index.html.twig', $data);
     }
 }
---
+++ src/Rottenwood/BarchartBundle/Service/BarchartParserService.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Author: Rottenwood
+ * Date Created: 05.09.14 17:42
+ */
+
+namespace Rottenwood\BarchartBundle\Service;
+use Doctrine\ORM\EntityManager;
+use Sunra\PhpSimple\HtmlDomParser;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Парсер данных с сайта barchart.com
+ * @date 05.09.2014
+ * @package Rottenwood\BarchartBundle\Service
+ */
+class BarchartParserService {
+    private $em;
+    private $kernel;
+    private $config;
+    private $tableTech;
+
+    public function __construct(EntityManager $em, Kernel $kernel) {
+        $this->em = $em;
+        $this->kernel = $kernel;
+        $this->config = $this->configLoad();
+    }
+
+    private function configLoad() {
+        $path = $this->kernel->locateResource("@RottenwoodBarchartBundle/Resources/config/barchart.yml");
+        if (!is_string($path)) {
+            throw new Exception("$path должен быть строкой.");
+        }
+        $config = Yaml::parse(file_get_contents($path));
+
+        return $config;
+    }
+
+    private function parsePage($url, $symbol) {
+        $page = HtmlDomParser::file_get_html($url . $symbol);
+
+        return $page;
+    }
+
+    private function parseIndicator($tr) {
+        $tableTech = $this->tableTech;
+        $indicator = array();
+
+        $indicator[] = $tableTech->find('tr', $tr)->find('td', 1)->plaintext;
+        $indicator[] = $tableTech->find('tr', $tr)->find('td', 2)->plaintext;
+        $indicator[] = $tableTech->find('tr', $tr)->find('td', 3)->plaintext;
+
+        return $indicator;
+    }
+
+    public function getPrice($symbol) {
+        $url = $this->config["url"]["price"];
+        $urlTechnical = $this->config["url"]["technical"];
+
+        $html = $this->parsePage($url, $symbol);
+        $htmlTechnical = $this->parsePage($urlTechnical, $symbol);
+
+        // Обработка таблицы цен
+        $table = $html->find('table#main-content table table', 0);
+
+        $title = $html->find('h1#symname', 0)->innertext;
+
+        // Расчет даты экспирации
+        $titleArray = explode(' ', $title);
+        end($titleArray);
+        $year = prev($titleArray);
+        $month = date_parse(prev($titleArray))["month"];
+
+        // Значения таблицы цен
+
+        $priceArray = array(
+            "Symbol" => $symbol,
+            "Title" => $title,
+            "Price" => $html->find('div#divQuotePageHeader span#dtaLast', 0)->plaintext,
+            "Commodity" => $titleArray[0],
+            "Expiration" => $month . "." . $year,
+            "Date" => date("m.Y"),
+            "Time" => $html->find('div#divQuotePageHeader span#dtaDate', 0)->plaintext,
+            "TimeLocal" => date("g:iA T"),
+            "High" => $table->find('tr', 1)->find('td', 1)->plaintext,
+            "Low" => $table->find('tr', 1)->find('td', 3)->plaintext,
+            "Open" => $table->find('tr', 3)->find('strong', 0)->plaintext,
+            "Close" => $table->find('tr', 3)->find('strong', 1)->plaintext,
+            "52WHigh" => $table->find('tr', 2)->find('span', 0)->plaintext,
+            "52WLow" => $table->find('tr', 2)->find('span', 4)->plaintext,
+            "Volume" => $table->find('tr', 4)->find('td#dtaVolume', 0)->plaintext,
+            "OpenInterest" => $table->find('tr', 4)->find('strong', 0)->plaintext,
+            "WeightedAlpha" => $table->find('tr', 5)->find('strong', 0)->plaintext,
+            "StandartDev" => $table->find('tr', 5)->find('strong', 1)->plaintext,
+            "20DAverage" => $table->find('tr', 6)->find('strong', 0)->plaintext,
+            "100DAverage" => $table->find('tr', 6)->find('strong', 1)->plaintext,
+            "14DRelStrength" => $table->find('tr', 7)->find('strong', 0)->plaintext,
+            "14DStochastic" => $table->find('tr', 7)->find('strong', 1)->plaintext,
+            "Trend" => $table->find('tr', 8)->find('td', 1)->plaintext,
+            "TrendStrength" => $table->find('tr', 8)->find('td', 3)->plaintext,
+        );
+
+        // Очистка данных от лишних пробелов
+        $priceArray = $this->trimArray($priceArray);
+
+        // Индикаторы
+        $this->tableTech = $htmlTechnical->find('div.mpbox', 1)->find('table', 0);
+
+        // Наполнение массива с индикаторами
+        $technicalDataArray = array();
+        $technicalDataArray = $this->parseShortTermIndicators($technicalDataArray);
+        $technicalDataArray = $this->parseMidTermIndicators($technicalDataArray);
+        $technicalDataArray = $this->parseLongTermIndicators($technicalDataArray);
+        $technicalDataArray = $this->parseOverallIndicators($technicalDataArray);
+
+        $priceArray["indicator"] = $this->purifyArray($technicalDataArray);
+
+        return $priceArray;
+    }
+
+    private function parseShortTermIndicators($technicalDataArray = array()) {
+        $technicalDataArray["s.7-AD"] = $this->parseIndicator(4);
+        $technicalDataArray["s.10-8-MAHiloChannel"] = $this->parseIndicator(5);
+        $technicalDataArray["s.20-MAvsPrice"] = $this->parseIndicator(6);
+        $technicalDataArray["s.20-50-MACD"] = $this->parseIndicator(7);
+        $technicalDataArray["s.20-Bollinger"] = $this->parseIndicator(8);
+
+        return $technicalDataArray;
+    }
+
+    private function parseMidTermIndicators($technicalDataArray = array()) {
+        // Среднесрочные индикаторы
+        $technicalDataArray["m.40-CCI"] = $this->parseIndicator(14);
+        $technicalDataArray["m.50-MAvsPrice"] = $this->parseIndicator(15);
+        $technicalDataArray["m.20-100-MACD"] = $this->parseIndicator(16);
+        $technicalDataArray["m.50-ParabolicTimePrice"] = $this->parseIndicator(17);
+
+        return $technicalDataArray;
+    }
+
+    private function parseLongTermIndicators($technicalDataArray = array()) {
+        // Долгосрочные индикаторы
+        $technicalDataArray["l.60-CCI"] = $this->parseIndicator(23);
+        $technicalDataArray["l.100-MAvsPrice"] = $this->parseIndicator(24);
+        $technicalDataArray["l.50-100-MACD"] = $this->parseIndicator(25);
+
+        return $technicalDataArray;
+    }
+
+    private function parseOverallIndicators($technicalDataArray = array()) {
+        // Общие индикаторы
+        $technicalDataArray["TrendSpotter"] = $this->parseIndicator(1);
+        $shortTermAverage = explode("&nbsp;", trim($this->tableTech->find('tr', 10)->find('td', 0)->plaintext));
+        $technicalDataArray["ShortTermAverage"] = array($shortTermAverage[1]);
+        $midTermAverage = explode("&nbsp;", trim($this->tableTech->find('tr', 19)->find('td', 0)->plaintext));
+        $technicalDataArray["MidTermAverage"] = array($midTermAverage[1]);
+        $longTermAverage = explode("&nbsp;", trim($this->tableTech->find('tr', 27)->find('td', 0)->plaintext));
+        $technicalDataArray["LongTermAverage"] = array($longTermAverage[1]);
+        $overallAverate = explode("&nbsp;", trim($this->tableTech->find('tr', 30)->find('td', 0)->plaintext));
+        $technicalDataArray["OverallAverage"] = array($overallAverate[1]);
+
+        return $technicalDataArray;
+    }
+
+    private function trimArray($array = array()) {
+        // Очистка массива от лишних пробелов
+        foreach ($array as $key => $value) {
+            $array[$key] = trim(preg_replace("/\s+/", " ", $array[$key]));
+        }
+
+        return $array;
+    }
+
+    private function purifyArray($array = array()) {
+        // Очистка массивов от лишних данных, пересборка массива
+        $pureArray = array();
+        foreach ($array as $indicatorName => $indicatorValue) {
+            foreach ($indicatorValue as $key => $value) {
+                if ($value == '&nbsp;') {
+                    unset($array[$indicatorName][$key]);
+                } else {
+                    $pureArray[$indicatorName] = $value;
+                }
+            }
+        }
+
+        return $pureArray;
+    }
+
+}
