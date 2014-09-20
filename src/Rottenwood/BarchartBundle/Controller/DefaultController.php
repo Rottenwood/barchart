d16a8b4a0f33563b62cf62249f85249ebac59557
Petr Karmashev (Rottenwood) <petr@quank.org>
2014-09-20 18:44:15 +0400

    Работа над сервисом аналитики

--- src/Rottenwood/BarchartBundle/Controller/DefaultController.php
+++ src/Rottenwood/BarchartBundle/Controller/DefaultController.php
@@ -9,8 +9,11 @@ class DefaultController extends Controller {
     public function indexAction() {
         $data = array();
         $parser = $this->get("barchart.parser");
+        $analizer = $this->get("barchart.analizer");
 
         $parser->saveAllFutures();
+
+//        $analizer->analyseOverallCorn();
 
         return $this->render('RottenwoodBarchartBundle:Default:index.html.twig', $data);
     }
--- src/Rottenwood/BarchartBundle/Service/AnalizerService.php
+++ src/Rottenwood/BarchartBundle/Service/AnalizerService.php
@@ -7,6 +7,7 @@
 namespace Rottenwood\BarchartBundle\Service;
 
 use Doctrine\ORM\EntityManager;
+use Rottenwood\BarchartBundle\Entity\Price;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
@@ -14,8 +15,13 @@ use Symfony\Component\HttpKernel\Kernel;
  * @package Rottenwood\BarchartBundle\Service
  */
 class AnalizerService {
+
     private $em;
     private $kernel;
+    private $high = 0;
+    private $highId = 0;
+    private $low = 0;
+    private $lowId = 0;
 
     public function __construct(ConfigService $configService, EntityManager $em, Kernel $kernel) {
         $this->em = $em;
@@ -29,14 +35,64 @@ class AnalizerService {
             $cornPrice = $corn->getPrice();
             $cornOverall = $corn->getOverall();
             var_dump($cornOverall);
+
+            // Удаление первой цены
+            array_shift($corns);
+
+            // Разница сравниваемой цены с последующими
             foreach ($corns as $cornToCheck) {
-                $cornPriceCheck = $cornToCheck->getPrice();
-                $priceDiff = $cornPrice - $cornPriceCheck;
-                echo $priceDiff, '..';
+//                $cornPriceCheck = $cornToCheck->getPrice();
+//                $priceDiff = $cornPrice - $cornPriceCheck;
+//                echo $priceDiff, '..';
+
+                var_dump($this->analyseProfit($corn, $cornToCheck, 'buy'));
+
             }
             var_dump('***************');
         }
 
-        return true;
+        return $this;
+    }
+
+    /**
+     * Расчет результата торговой позиции
+     * @param Price  $priceObject
+     * @param Price  $priceCompareObject
+     * @param string $direction
+     * @return array
+     */
+    protected function analyseProfit($priceObject, $priceCompareObject, $direction) {
+        $return = array();
+
+        $openPrice = $priceObject->getPrice();
+        $closePrice = $priceCompareObject->getPrice();
+
+        //        if ($direction == 'buy') {}
+        $profit = $closePrice - $openPrice;
+
+        if ($this->high < $profit) {
+            $this->high = $profit;
+            $this->highId = $priceCompareObject->getId();
+        }
+
+        if ($this->low > $profit) {
+            $this->low = $profit;
+            $this->lowId = $priceCompareObject->getId();
+        }
+
+        $timePassed = $priceCompareObject->getUnixtime() - $priceObject->getUnixtime();
+
+        $return['profit'] = $profit;
+        $return['open'] = $openPrice;
+        $return['close'] = $closePrice;
+        $return['highProfit'] = $this->high;
+        $return['highProfitObjectId'] = $this->highId;
+        $return['lowProfit'] = $this->low;
+        $return['lowProfitObjectId'] = $this->lowId;
+        $return['timePassed'] = $timePassed;
+        $return['openObjectId'] = $priceObject->getId();
+        $return['closeObjectId'] = $priceCompareObject->getId();
+
+        return $return;
     }
 }
