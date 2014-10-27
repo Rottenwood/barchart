89674694a368368e332a8d1ae3e930ecb8176314
Petr Karmashev <smonkl@bk.ru>
2014-10-27 11:04:21 +0400

    В сущность Сделка добавлены константы с номерами символов, и массив соответствия номеров с сущностями символов

--- src/Rottenwood/BarchartBundle/Entity/Trade.php
+++ src/Rottenwood/BarchartBundle/Entity/Trade.php
@@ -12,6 +12,26 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\HasLifecycleCallbacks
  */
 class Trade {
+    const DIRECTION_BUY = 1;
+    const DIRECTION_SELL = -1;
+
+    const SYMBOL_FUTURES_CORN = 1;
+    const SYMBOL_FUTURES_CRUDEOIL = 2;
+    const SYMBOL_FUTURES_DJMINI = 3;
+    const SYMBOL_FUTURES_EMINI = 4;
+    const SYMBOL_FUTURES_GOLD = 5;
+    const SYMBOL_FUTURES_NATURALGAS = 6;
+    const SYMBOL_FUTURES_SILVER = 7;
+    const SYMBOL_FUTURES_SOYBEANS = 8;
+    const SYMBOL_FUTURES_WHEAT = 9;
+
+    const SYMBOL_FOREX_AUDUSD = 50;
+    const SYMBOL_FOREX_EURUSD = 51;
+    const SYMBOL_FOREX_GBPUSD = 52;
+    const SYMBOL_FOREX_USDCAD = 53;
+    const SYMBOL_FOREX_USDCHF = 54;
+    const SYMBOL_FOREX_USDJPY = 55;
+
     /**
      * @var integer
      *
@@ -29,18 +49,18 @@ class Trade {
     private $account;
 
     /**
-     * @var string
+     * @var int
      *
-     * @ORM\Column(name="symbol", type="string", length=255)
+     * @ORM\Column(name="symbol", type="smallint")
      */
     private $symbol;
 
     /**
-     * @var bool
+     * @var int
      *
-     * @ORM\Column(name="direction", type="boolean")
+     * @ORM\Column(name="direction", type="smallint")
      */
-    private $directionBuy;
+    private $direction;
 
     /**
      * @var float
@@ -150,17 +170,17 @@ class Trade {
     }
 
     /**
-     * @return boolean
+     * @return int
      */
-    public function isDirectionBuy() {
-        return $this->directionBuy;
+    public function getDirection() {
+        return $this->direction;
     }
 
     /**
-     * @param boolean $directionBuy
+     * @param int $direction
      */
-    public function setDirectionBuy($directionBuy) {
-        $this->directionBuy = $directionBuy;
+    public function setDirection($direction) {
+        $this->direction = $direction;
     }
 
     /**
@@ -308,5 +328,30 @@ class Trade {
      */
     public function setOpenDate($openDate) {
         $this->openDate = $openDate;
+    }
+
+    /**
+     * Получение массива названий сущностей для торговых символов
+     * @return array
+     */
+    public function getSymbolName() {
+        return array(
+            self::SYMBOL_FUTURES_CORN => 'Corn',
+            self::SYMBOL_FUTURES_CRUDEOIL => 'CrudeOil',
+            self::SYMBOL_FUTURES_DJMINI => 'DJMini',
+            self::SYMBOL_FUTURES_EMINI => 'Emini',
+            self::SYMBOL_FUTURES_GOLD => 'Gold',
+            self::SYMBOL_FUTURES_NATURALGAS => 'NaturalGas',
+            self::SYMBOL_FUTURES_SILVER => 'Silver',
+            self::SYMBOL_FUTURES_SOYBEANS => 'Soybeans',
+            self::SYMBOL_FUTURES_WHEAT => 'Wheat',
+
+            self::SYMBOL_FOREX_AUDUSD => 'AUDUSD',
+            self::SYMBOL_FOREX_EURUSD => 'EURUSD',
+            self::SYMBOL_FOREX_GBPUSD => 'GBPUSD',
+            self::SYMBOL_FOREX_USDCAD => 'USDCAD',
+            self::SYMBOL_FOREX_USDCHF => 'USDCHF',
+            self::SYMBOL_FOREX_USDJPY => 'USDJPY',
+        );
     }
 }
