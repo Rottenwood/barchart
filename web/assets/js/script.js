96b2538c09585af37a89c13bedcde1000e52634b
Petr Karmashev <smonkl@bk.ru>
2014-11-27 11:49:46 +0300

    Работа над обработкой формы создания стратегии

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+    vars = {
+        indicatorsCount: {}
+    };
+
     // Форма создания стратегии
     if ($('div#strategy').length) {
         $('button#strategy_addSignal').off("click").on("click", function () {
@@ -8,7 +12,6 @@ $(document).ready(function () {
 
         bindOnAddIndicatorEvent();
     }
-
 
 
 });
@@ -25,8 +28,7 @@ $(document).ready(function () {
 function addSignal() {
     var $strategySignals = $('div#strategy-signals'),
         prototype = $strategySignals.closest('[data-prototype]'),
-        id = $(prototype).attr('id'),
-        newSignalDiv = $(prototype).data('prototype');
+        newSignalDiv = $(prototype).data('prototype').replace(/__name__/g, $strategySignals.children().length);
 
     $strategySignals.append(newSignalDiv);
 
@@ -37,16 +39,18 @@ function addSignal() {
 function addIndicator($div) {
     var $signalIndicators = $('div#strategy-signals div[id$="_indicators"]'),
         prototype = $('div#strategy-signals').closest('[data-prototype-indicators]'),
-        id = $(prototype).attr('id'),
-        newIndicatorDiv = $(prototype).data('prototype');
+        newIndicatorDiv = $(prototype).data('prototype-indicators').replace(/__name__/g, $signalIndicators.children().length);
 
-    $div.append('<p>test</p>');
+    // TODO: $('#strategy_signals_0_addIndicator').parent('div').prev('div'); // прототип для индикаторов
+
+    console.log(newIndicatorDiv);
+
+    $div.append(newIndicatorDiv);
 }
 
 // Обработка события
 function bindOnAddIndicatorEvent() {
     $('button.addIndicator').off("click").on("click", function (event) {
-        console.log('test');
         addIndicator($(event.target).parent('div').prev('div').children('div'));
     });
 }
