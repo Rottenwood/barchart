63cead3eb3fcb4d574401a43ae1efbaeea69be51
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-02-19 22:54:02 +0300

    Фикс бага при создании стратегии

--- src/Rottenwood/BarchartBundle/Entity/Strategy.php
+++ src/Rottenwood/BarchartBundle/Entity/Strategy.php
@@ -92,6 +92,7 @@ class Strategy extends Symbol {
      */
     public function prePersistCallback() {
         $this->setCreatedAt(new \Datetime());
+        $this->setUpdatedAt(new \Datetime());
     }
 
     /**
